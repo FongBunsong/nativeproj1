25f388dc52607bc88c929b59156250e0e26941a3
fongbunsong <macbook@macbooks-MacBook-Pro.local>
2025-02-04 09:51:27 +0700

    first commit

---
+++ 01_dbconnect.js
@@ -0,0 +1,26 @@
+// STEP-1 : IMPORT MONGOOSE PACKAGE
+const mongoose = require('mongoose');
+
+// Database Connection URL
+//Mongoose is an Object Document Mapper (ODM)
+const url = 'mongodb+srv://user123:user123@cluster0.05ujz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// STEP-2 : ESTABLISH CONNECTION WITH MONGODB DATABASE THROUGH MONGOOSE
+// err is callback function Parameter. ARROW OPERATOR.
+// JSON.stringify convert Object to String. 2 means Indentation of Two space Character 
+const dbconnect = () => {
+mongoose.connect(url)
+      .then( () => 
+             {
+               console.log('NODEJS TO MongoDB Connection ESTABLISH.....');
+             })
+      .catch( err => 
+              {
+               console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+               process.exit();
+              }); 
+            }
+
+    
+// STEP-3 : EXPORT MODULE mongoose because we need it in other JS file
+module.exports = dbconnect;
